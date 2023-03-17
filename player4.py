a68564f596bae22919cadc58c5c521456b6902e2
Cross, Winter Isaac <>
2023-03-17 10:39:29 -0400

    moved some functions to player4

--- player4.py
+++ player4.py
@@ -110,6 +110,118 @@ class Player4(Player3):
                     else:# we can win
                         return utils.worstCard(likelyToWinCards, trump, ledSuit)
 
+    def getCardsThatAreLikelyToWin(self, trick,playersInTrick,team,otherTeam,caller,trump,cards):
+        ledSuit = self.getCardSuit(trick[0], trump)
+        cardsThatWouldWin = []
+        bestCard = utils.best_card(trick)
+        for card in cards:
+            if card == utils.best_card([card,bestCard], trump, ledSuit):
+                cardsThatWouldWin.append(card)
+        if len(cardsThatWouldWin) == 0:
+            # we cannot win
+            return []
+        otherTeamStillHasToGo = False
+        for person in otherTeam:
+            if (person not in playersInTrick) and (person not in person.game._inactives):
+                #player has not gone and is not inactive
+                otherTeamStillHasToGo = True
+        if not otherTeamStillHasToGo:
+            #no one on the other team still has to go
+            return cardsThatWouldWin
+        #someone on the other team still has to go
+        if ledSuit == trump:
+            #lead was trump
+            if caller in otherTeam:
+                #other team called
+                right = utils.findCardInCards(cardsThatWouldWin,'J',trump)
+                if right!=None:
+                    #this is the right
+                    return [right]
+                return []
+            return cardsThatWouldWin#!!!
+        #ledSuit is not trump
+        ace = utils.findCardInCards(cardsThatWouldWin, 'A', ledSuit)
+        if ace!=None:
+            return [ace]
+        if utils.hasSuit(cardsThatWouldWin,trump,trump):
+            return cardsThatWouldWin
+        #We don't have shit
+        return [utils.best_card(cardsThatWouldWin,trump,ledSuit)]#low chance of working, but give it a try
+
+    def myTeamIsLikelyToWin(self,trick,playersInTrick,team,trump,player,caller):
+        #!!!must consider strength of partner's card and who all has gone
+        #has our partner played yet?
+        partner = None
+        for person in team:
+            if person!=player:
+                partner = person
+        if partner in playersInTrick:
+            #partner has played
+            if self.myTeamIsWinning(trick, playersInTrick, team, trump):
+                # we are currently winning
+                return True#!!!
+            else:
+                # we are currently losing
+                return False
+        else:
+            #partner has not played
+            ledCard = trick[0]
+            if utils.getCardSuit(ledCard,trump)==trump:
+                #foe led trump
+                if caller==partner:
+                    #partner called
+                    return True
+                else:
+                    #partner did not call
+                    return False
+            else:
+                if (ledCard[0]=='9') or (ledCard[0]=='T') or (ledCard[0]=='J') or (ledCard[0]=='Q'):
+                    return True#!!!
+                else:
+                    return False
+
+
+    def getLikelyOutrides(self,cards,trump):
+        #!!!this function will need to be updated and improved
+        # find offsuits that can go all the way through
+        outrides = []
+        greenSuits = utils.getGreenSuits(trump)
+        for suit in greenSuits:
+            cardsOfSuit = utils.getCardsOfSuit(cards, suit, trump)
+            #!!!this does not take into account how far we are into the round
+            if len(cardsOfSuit) > 0:
+                if len(cardsOfSuit) <= 3:
+                    # play the ace
+                    ace = utils.findCardInCards(cardsOfSuit, 'A', suit)
+                    if ace != None:
+                        outrides.append(ace)
+                if len(cardsOfSuit) == 1:
+                    # play the king
+                    king = utils.findCardInCards(cardsOfSuit, 'K', suit)
+                    if king != None:
+                        outrides.append(king)
+    #			if len(cardsOfSuit) == 1:  # !!!highly doubtful that this is a good idea
+    #				# play the queen
+    #				queen = findCardInCards(cardsOfSuit, 'Q', suit)
+    #				if queen != None:
+    #					outrides.append(queen)
+        otherSuit = utils.same_color(trump)
+        cardsOfOther = utils.getCardsOfSuit(cards, otherSuit, trump)
+        if len(cardsOfOther) > 0:
+            if len(cardsOfOther) <= 2:
+                # play the ace
+                ace = utils.findCardInCards(cardsOfOther, 'A', otherSuit)
+                if ace != None:
+                    outrides.append(ace)
+    #		if len(cardsOfOther) == 1:
+    #			# play the king
+    #			king = findCardInCards(cardsOfOther, 'K', otherSuit)
+    #			if king != None:
+    #				outrides.append(king)
+        return outrides
+
+
+
 """
             if utils.myTeamIsWinning(trick,playersInTrick,team,trump):  # Our team is winning
                 voidCard = utils.getVoidCard(cards, trump)
@@ -133,3 +245,5 @@ class Player4(Player3):
                     return utils.worstCard(myPlaysThatWin, trump, ledSuit)
                     # !!!worstCard may be better here. May have to split into cases
 """
+
+
