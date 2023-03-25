738fba77952bc80870f87e1efc74f25baf9eee72
Cross, Winter Isaac <>
2023-03-25 16:54:41 -0400

    turning the dials on player5

--- Player5.py
+++ Player5.py
@@ -1,20 +1,41 @@
 import utils
 from player4B import Player4B
 
+
+
 class Player5(Player4B):
+    callStrength = 2
+    lonerStrength = 2.5
+
     def call(self, topCard):
-        callStrength = 2
-        lonerStrength = 2.5
         handStrength = 0
+        numTrump = 0
         for card in self.game.hand_for(self):
             handStrength += self.getStrengthOfCard(card,topCard[1])
+            if utils.getCardSuit(card,topCard[1])==topCard[1]:
+                numTrump+=1
         position = self.game.get_player_position(self)
         if position ==3:
             #also find the strength of the topCard
             handStrength += self.getStrengthOfCard(topCard,topCard[1])
-        if handStrength>=lonerStrength:
+            if utils.getCardSuit(card,topCard[1])==topCard[1]:
+                numTrump+=1
+
+        #add points for higher number of trump
+        if numTrump ==1:
+            handStrength+=.1
+        elif numTrump ==2:
+            handStrength+=.3
+        elif numTrump == 3:
+            handStrength+=.5
+        elif numTrump == 4:
+            handStrength +=1
+        elif numTrump >= 5:
+            handStrength += 1.5
+
+        if handStrength>=self.lonerStrength:
             return "alone"
-        elif handStrength>=callStrength:
+        elif handStrength>=self.callStrength:
             return True
         else:
             return False
@@ -24,14 +45,28 @@ class Player5(Player4B):
         suits = utils.getSuits()
         suits.remove(bannedSuit)
         for suit in suits:
-            callStrength = 2
-            lonerStrength = 2.5
             handStrength = 0
+            numTrump = 0
             for card in self.game.hand_for(self):
                 handStrength += self.getStrengthOfCard(card, suit)
-            if handStrength >= lonerStrength:
+                if utils.getCardSuit(card, suit) == suit:
+                    numTrump += 1
+
+            # add points for higher number of trump
+            if numTrump == 1:
+                handStrength += .1
+            elif numTrump == 2:
+                handStrength += .3
+            elif numTrump == 3:
+                handStrength += .5
+            elif numTrump == 4:
+                handStrength += 1
+            elif numTrump >= 5:
+                handStrength += 1.5
+
+            if handStrength >= self.lonerStrength:
                 return [suit,"alone"]
-            elif handStrength >= callStrength:
+            elif handStrength >= self.callStrength:
                 return [suit,True]
         position = self.game.get_player_position(self)
         if position ==3:
@@ -40,7 +75,7 @@ class Player5(Player4B):
         return [None,False]
 
 
-    def getStrengthOfCard(self,card, trump):
+    def getStrengthOfCard(self,card,trump):
         #a general heuristic for the strength of a card when calling trump
         if utils.getCardSuit(card,trump)==trump:
             #this is trump
@@ -54,9 +89,9 @@ class Player5(Player4B):
             elif card[0]=='A':
                 return .6
             elif card[0]=='K':
-                return .3
+                return .4
             elif card[0]=='Q':
-                return .2
+                return .3
             elif card[0]=='T':
                 return .2
             elif card[0]=='9':
@@ -64,9 +99,9 @@ class Player5(Player4B):
         else:
             #this is not trump
             if card[0]=='A':
-                return .5
+                return .3
             elif card[0]=='K':
-                return .2
+                return .1
         return 0
 
 
